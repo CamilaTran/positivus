40d8ce7162b5771dbd2dd9db94dc439a5ce3990c
coolkid!playcode <161105950+CamilaTran@users.noreply.github.com>
2025-04-10 12:15:03 +0700

    setup

---
+++ app/ui/font.ts
@@ -0,0 +1,8 @@
+import { Space_Grotesk } from "next/font/google";
+
+const groteskFont = Space_Grotesk({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export { groteskFont };
