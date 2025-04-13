6e527f23376681912b1dc46fbb9d055eb2d019e3
coolkid!playcode <161105950+CamilaTran@users.noreply.github.com>
2025-04-13 18:48:50 +0700

    done team components

--- features/home/components/member-card/index.tsx
+++ features/home/components/member-card/index.tsx
@@ -18,13 +18,18 @@ const MemberCard = ({ name, position, experience, image, linkedLink }: MemberCar
     return (
         <article className="border-1 border-b-4 border-black py-10 rounded-[45px] px-9 w-fit">
             <div className="flex gap-5">
-                <div className="relative w-[98px] h-[98px] mask">
-                    <Image src={image} alt={`Profile image ${name}`} className="mask" fill/>
-                    <div className="absolute inset-0 bg-[#A7DF63] mix-blend-multiply opacity-80 pointer-events-none rounded-none" />
-                    <Image src="/team/flower-mask.png" fill alt="flower-below" className="absolute -z-1 right-1"  />
+                <div className="relative w-[98px] h-[98px]">
+                    <div className="mask">
+                        <Image src={image} alt={`Profile image ${name}`} width={98} height={98} />
+                        <div className="absolute inset-0 bg-[#A7DF63] mix-blend-multiply opacity-80 pointer-events-none rounded-none" />
+                    </div>
+
+                    <div className="absolute -z-1 -right-2 top-2">
+                        <Image src="/team/flower-mask.png" alt="flower-below" width={98} height={98} />
+                    </div>
                 </div>
                 <div className="flex flex-col justify-between flex-1">
-                    <Link href={linkedLink} aria-label={`Navigate to linked profile of ${name}`} className="">
+                    <Link href={linkedLink} aria-label={`Navigate to linked profile of ${name}`} className="flex justify-end">
                         <LinkedIcon bgColor="black" textColor="#B9FF66" />
                     </Link>
 
